395bb1c463296ddfb23fa695a86a0df794d1a3a4
CHARAN SRINIVAS <charansrinivassajja@gmail.com>
2022-12-04 19:35:25 +0530

    Updated json files

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
-export const spin_hours =[3,9,15,21]
-export const spin_minute = 15
+export const spin_hours =[3,9,19,21]
+export const spin_minute = 35
 export const next_spin_delay = 20
 
 export default{
